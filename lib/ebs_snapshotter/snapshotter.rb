518fc83544b04f58d5da0ce470a9a5b4ec239a83
Erling Wegger Linde <erlingwl@gmail.com>
2013-09-02 21:52:22 +0200

    actually save the snapshot

--- lib/ebs_snapshotter/snapshotter.rb
+++ lib/ebs_snapshotter/snapshotter.rb
@@ -10,12 +10,17 @@ module EbsSnapshotter
     end
 
     def snapshot
+      new_snapshot = connection.snapshots.new
+      new_snapshot.description = "#{@volume_id} - #{Time.now}"
+      new_snapshot.volume_id = @volume_id
+      saved = new_snapshot.save
+      raise "Failed to save snapshot for #{@volume_id}" unless saved 
     end
 
     private
 
       def connection
-        @connection ||= Fog::Storage.new(@aws_config[:fog])
+        @connection ||= Fog::Compute.new(@aws_config[:fog])
       end
 
   end
